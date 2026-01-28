316a48e07c0127f11b8949c71dd51b3623510c97
angorita <oaangarita@yahoo.com.ar>
2026-01-28 15:02:17 -0300

    siguiente commit

---
+++ ejercicios/ejercicio01.go
@@ -0,0 +1,15 @@
+package ejercicios
+
+import "strconv"
+
+func Convertir(texto string) (int, string) {
+	num, err := strconv.Atoi(texto)
+	if err != nil {
+		return 0, "Hubo un error" + err.Error()
+	}
+	if num > 100 {
+		return num, "Es mayor de 100"
+	} else {
+		return num, "Es menor de 100"
+	}
+}
--- main.go
+++ main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	// "runtime"
 
-	"github.com/angorita/aaa/variables"
+	"github.com/angorita/aaa/ejercicios"
+	// "github.com/angorita/aaa/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
-	variables.RestoVariables()
-	texto, estado := variables.ConviertoATexto(93)
-	fmt.Println(texto, estado)
+	// variables.MuestroEnteros()
+	// variables.RestoVariables()
+	// texto, estado := variables.ConviertoATexto(93)
+	// fmt.Println(texto, estado)
+	// if os := runtime.GOOS; os == "linux" || os == "Os" {
+	// 	fmt.Println("Es Linux")
+
+	// } else {
+	// 	fmt.Println("Es windows")
+	// }
+	// switch os := runtime.GOOS; os {
+	// case "linux":
+	// 	fmt.Println("Linux")
+	// case "windows":
+	// 	fmt.Println("windows")
+	// default:
+	// 	fmt.Printf("%s\n", os)
+	// }
+	num, texto := ejercicios.Convertir("127")
+	fmt.Println(num)
+	fmt.Println(texto)
 }
